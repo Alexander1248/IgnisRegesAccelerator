0e4a46d04d257d0351874744c9ce5505935af7aa
Alexander <sasha.izmyloff@mail.ru>
2024-07-06 19:56:31 +0300

    Unity localization system now used in dialogue system + some fixes

---
+++ Assets/Scripts/Controllers/ShipController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Controllers
+{
+    public class ShipController : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+        void Update()
+        {
+        
+        }
+    }
+}
